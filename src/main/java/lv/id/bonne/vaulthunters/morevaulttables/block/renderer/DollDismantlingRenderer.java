e5fd3dfcb59478af5fbf4c7637f3b0fb5e067481
BONNe <bonne@bonne.id.lv>
2024-09-23 15:56:24 +0300

    Clean up DollDismantlingRenderer

--- src/main/java/lv/id/bonne/vaulthunters/morevaulttables/block/renderer/DollDismantlingRenderer.java
+++ src/main/java/lv/id/bonne/vaulthunters/morevaulttables/block/renderer/DollDismantlingRenderer.java
@@ -1,28 +1,21 @@
 package lv.id.bonne.vaulthunters.morevaulttables.block.renderer;
 
 
-import com.mojang.authlib.GameProfile;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Vector3f;
 import org.jetbrains.annotations.NotNull;
 
 import iskallia.vault.entity.entity.DollMiniMeEntity;
-import iskallia.vault.init.ModEntities;
-import iskallia.vault.item.VaultDollItem;
-import lv.id.bonne.vaulthunters.morevaulttables.block.JewelSelectorTableBlock;
 import lv.id.bonne.vaulthunters.morevaulttables.block.entity.DollDismantlingTileEntity;
-import lv.id.bonne.vaulthunters.morevaulttables.block.entity.JewelSelectorTableTileEntity;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.item.ItemStack;
 
 
+/**
+ * This class manages block entity rendering
+ */
 public class DollDismantlingRenderer implements BlockEntityRenderer<DollDismantlingTileEntity>
 {
     public DollDismantlingRenderer(BlockEntityRendererProvider.Context context)
@@ -59,14 +52,13 @@ public class DollDismantlingRenderer implements BlockEntityRenderer<DollDismantl
         float result = totalItemsInDoll * 1f / (amount == 0 ? Integer.MAX_VALUE : amount);
         y *= result;
 
-
         // Position the player model inside the block
         poseStack.pushPose();
-        poseStack.translate(0.5, y, 0.5); // Adjust position to center inside the block
-        poseStack.scale(0.6f, 0.6f, 0.6f); // Scale down the player model to fit the block
+        poseStack.translate(0.5, y, 0.5);
+        poseStack.scale(0.6f, 0.6f, 0.6f);
 
         long gameTime = tileEntity.getLevel().getGameTime();
-        float rotationAngle = (gameTime % 360L) * 1.0f;
+        float rotationAngle = (gameTime % 360L) * 25.0f;
 
         poseStack.mulPose(Vector3f.YP.rotationDegrees(rotationAngle));
 
@@ -76,7 +68,5 @@ public class DollDismantlingRenderer implements BlockEntityRenderer<DollDismantl
             render(miniMeEntity, 0.0f, partialTicks, poseStack, buffer, combinedLight);
 
         poseStack.popPose();
-
-
     }
 }
