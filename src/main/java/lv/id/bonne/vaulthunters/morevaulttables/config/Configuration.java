8d3d3c4d3eca46351bff0a58afb842b4d455fcc2
BONNe <bonne@bonne.id.lv>
2024-09-25 11:05:37 +0300

    Fixes config file.

--- src/main/java/lv/id/bonne/vaulthunters/morevaulttables/config/Configuration.java
+++ src/main/java/lv/id/bonne/vaulthunters/morevaulttables/config/Configuration.java
@@ -23,34 +23,34 @@ public class Configuration
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
         this.blockVaultDolls = builder.
-            comment("This option allows to toggle if Vault Dolls should be able to place in the world.").
-            comment("By default: false - does not blocks vault doll placement").
+            comment("This option allows to toggle if Vault Dolls should be able to place in the world.",
+                "By default: false - does not blocks vault doll placement").
             define("block-vault-dolls", false);
 
         this.dollDismantlerEnergyStorage = builder.
-            comment("This option allows to change the amount of energy that Doll Dismantler stores inside block.").
-            comment("By default: 1000").
+            comment("This option allows to change the amount of energy that Doll Dismantler stores inside block.",
+                "By default: 1000").
             defineInRange("doll-dismantler-energy-storage", 1000, 0, Integer.MAX_VALUE);
 
         this.dollDismantlerEnergyConsumption = builder.
-            comment("This option allows to change the amount of energy that Doll Dismantler uses per tick.").
-            comment("By default: 16").
+            comment("This option allows to change the amount of energy that Doll Dismantler uses per tick.",
+                "By default: 16").
             defineInRange("doll-dismantler-energy-consumption", 16, 0, Integer.MAX_VALUE);
 
         this.dollDismantlerEnergyTransfer = builder.
-            comment("This option allows to change the amount of energy that Doll Dismantler can receive/extract.").
-            comment("By default: 100").
+            comment("This option allows to change the amount of energy that Doll Dismantler can receive/extract.",
+                "By default: 100").
             defineInRange("doll-dismantler-energy-transfer", 100, 0, Integer.MAX_VALUE);
 
         this.dollDismantlerVolume = builder.
-            comment("This option allows to change the sound for Doll Dismantler operation.").
-            comment("By default: 0.5").
+            comment("This option allows to change the sound for Doll Dismantler operation.",
+                "By default: 0.5").
             defineInRange("doll-dismantler-volume", 0.5f, 0, 1f);
 
         this.dollDismantlerRotationSpeed = builder.
-            comment("This option allows to change the rotation speed for Doll inside Dismantler.").
-            comment("By default: 25").
-            defineInRange("doll-dismantler-volume", 25d, 0, 2000);
+            comment("This option allows to change the rotation speed for Doll inside Dismantler.",
+                "By default: 25").
+            defineInRange("doll-dismantler-rotation-speed", 25d, 0, 2000);
 
         Configuration.GENERAL_SPEC = builder.build();
     }
